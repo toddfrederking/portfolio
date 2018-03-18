327b13e1bcd7839baf59034066c6cf5a23c38f0c
TODD FREDERKING <toddfrederking@gmail.com>
2018-03-18 17:41:15 -0500

    colors

--- portfoliotest/src/App.js
+++ portfoliotest/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import './assets/css/fonts.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Header">
+        <h1>Header</h1>
+        </div>
+
+
+        <div className="Skills">
+        <h1>Skills</h1>
+        </div>
+
+
+        <div className="Projects">
+        <h1>Projects</h1>
+        </div>
+
+
+        <div className="About">
+        <h1>About</h1>
+        </div>
+
+
+        <div className="Contact">
+        <h1>Contact</h1>
+        </div>
+
+
+        <div className="Footer">
+        <h1>Footer</h1>
+        </div>
+
       </div>
     );
   }
